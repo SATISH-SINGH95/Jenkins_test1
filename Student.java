cbda3be13e9d4180dc167d7d3a90b8f7c07234a7
Satish Kumar Singh <sk95satish@gmail.com>
2023-10-24 17:37:24 +0530

    6th commit
    6th commit

--- Student.java
+++ Student.java
@@ -2,6 +2,7 @@ public class Student {
    
     public void sayHello(){
         System.out.println("Hi, I am Student");
+        System.out.println("I am only for the testing purpose");
     }
     
 }

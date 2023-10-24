8b23942e9d280d75959ecd98dd55f33063fed3d8
Satish Kumar Singh <sk95satish@gmail.com>
2023-10-24 17:16:44 +0530

    initial commit
    added hello word and this is satish

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("This is simple java program to testing purpose");
         System.out.println(new Date());
+        System.out.println("Hello world!!");
+        System.out.println("This is satish");
     }
 
 }

f1485916c22213956c99e4376f45e7746b9a9630
satish <sk95satish@gmail.com>
2024-01-07 17:59:17 +0530

    Satish kumar singh line is added

--- Student.java
+++ Student.java
@@ -2,6 +2,7 @@ public class Student {
    
     public void sayHello(){
         System.out.println("Hi, I am Student");
+        System.out.println("My name is Satish Kumar Singh");
         System.out.println("I am only for the testing purpose");
     }
     

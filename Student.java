0456b468566a121d78dace26c84499dccffd6986
satish <sk95satish@gmail.com>
2024-01-07 18:10:42 +0530

    new line added

--- Student.java
+++ Student.java
@@ -4,6 +4,8 @@ public class Student {
         System.out.println("Hi, I am Student");
         System.out.println("My name is Satish Kumar Singh");
         System.out.println("I am only for the testing purpose");
+
+        System.out.println("Hello everyone from Satish side");
     }
     
 }
